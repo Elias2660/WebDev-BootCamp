b2194a0751ace97214f199db1dcd7b548b216a88
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2023-08-28 08:39:15 +0800

    new folder

--- backEnd/3.2 HTTP Requests/index.js
+++ backEnd/3.2 HTTP Requests/index.js
@@ -2,6 +2,21 @@ import express from "express";
 const app = express();
 const port = 3000
 
+//add code that sends a message back to the website
+app.get("/",(req, res) => {
+    res.send("<h1> Hello World </h1>");
+    console.log(req.rawHeaders);
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1> About Me </h1>");
+});
+
+
+app.get("/contact", (req, res) => {
+    res.send("<h1> Contact </h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server Running at Port ${port}`);
 });
